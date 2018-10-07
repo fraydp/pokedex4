07f53a925b73002510d8c6f87843cd3981ee9f5c
Fray Pineda <fraypineda@imac-de-fray.local>
2018-10-06 20:51:09 -0400

    con los primeros valores hasta video 89

--- Pokemon/Controller/PokemonDetailVC.swift
+++ Pokemon/Controller/PokemonDetailVC.swift
@@ -13,14 +13,45 @@ class PokemonDetailVC: UIViewController {
     var pokemon: Pokemon!
     
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var nextEvoImage: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         nameLbl.text = pokemon.name
+        pokemon.downloadPokemonDetails{
+            
+            print("#########Did arrive here?")
+            //Whatever we write will only be called after the network call is complete!
+            
+            self.updateUI()
+        }
+    }
+    
+    func updateUI() {
+        attackLbl.text = pokemon.attack
+        defenseLbl.text = pokemon.defense
+        heightLbl.text = pokemon.height
+        weightLbl.text = pokemon.weight
     }
 
   
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+    }
     
 
     
